e05edf06866bb1134412f14e4a199cf3d378739c
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-10 07:38:05 +0300

    edit

--- index.ts
+++ index.ts
@@ -1,20 +1,25 @@
 import { mapTo } from 'rxjs/operators';
 import { interval, race } from 'rxjs';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/race
+
 function getRandInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// old
+// interval(1500),
+// interval(1000).pipe(mapTo('1s won!')),
+// interval(2000),
+// interval(2500)
+
 //take the first observable to emit
-const example = race(
-  //emit every 1.5s
-  interval(1500),
-  //emit every 1s
-  interval(1000).pipe(mapTo('1s won!')),
-  //emit every 2s
-  interval(2000),
-  //emit every 2.5s
-  interval(2500)
+const src = race(
+  interval(2500).pipe(mapTo('2s won!')),
+  interval(1500).pipe(mapTo('1s won!'))
 );
+
+// interval(5000),
+// interval(3500)
 //output: "1s won!"..."1s won!"...etc
-const subscribe = example.subscribe(val => console.log(val));
+const subscribe = src.subscribe((val) => console.log(val));
