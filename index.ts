57430300e51b4fffc635f2418ef6be82c97e3abb
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-10 07:41:18 +0300

    try-3exmpl

--- index.ts
+++ index.ts
@@ -7,19 +7,27 @@ function getRandInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// old
+// old // expl - 1
 // interval(1500),
 // interval(1000).pipe(mapTo('1s won!')),
 // interval(2000),
 // interval(2500)
 
+// expl - 2
 //take the first observable to emit
-const src = race(
-  interval(2500).pipe(mapTo('2s won!')),
-  interval(1500).pipe(mapTo('1s won!'))
-);
+// const src = race(
+//   interval(2500).pipe(mapTo('2s won!')),
+//   interval(3500).pipe(mapTo('1s won!'))
+// );
 
 // interval(5000),
 // interval(3500)
-//output: "1s won!"..."1s won!"...etc
+
+// expl - 3
+//take the first observable to emit
+const src = race(
+  interval(getRandInt(500, 1000)).pipe(mapTo('2s won!')),
+  interval(getRandInt(1000, 3000)).pipe(mapTo('1s won!'))
+);
+
 const subscribe = src.subscribe((val) => console.log(val));
